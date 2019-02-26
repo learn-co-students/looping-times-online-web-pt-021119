0638046113044008eb723b0a9bd461b63c22d8a7
Taylor Mills <taylormills24@yahoo.com>
2019-02-26 00:50:46 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,5 +1,9 @@
-def using_times
-	#your code here
+using_times= 100
+
+7.times do 
+  puts "Wingardium Leviosa."
+  using_times = using_times-10
+  puts "Now there are only #{using_times} times left to count!"
 end
 
 

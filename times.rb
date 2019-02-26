cf67405e52a0b8fc5d47151858c3ac4d97d22363
Taylor Mills <taylormills24@yahoo.com>
2019-02-26 01:04:15 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,9 +1,5 @@
-using_times= 100
-
 7.times do 
-  puts "Wingardium Leviosa."
-  using_times = using_times-10
-  puts "Now there are only #{using_times} times left to count!"
+  puts "Wingardium Leviosa"
 end
 
 
